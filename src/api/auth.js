f5775dd9b5554732300e8dc8e082a24346bdaed4
Afra Fayas <afrafayask@gmail.com>
2026-02-14 17:24:10 +0400

    console

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,18 +1,21 @@
         import axios from 'axios';
 
-        export  const registerUser = async (userData) => {  
+        export  const registerUser = async (userData) => { 
+             
             // const API_URL = process.env.API_URL; 
 
             //new way to access env variables in Vite
             const API_URL = import.meta.env.VITE_API_URL;
+            
+            console.log("API URL:", API_URL);
 
             const response = await axios.post(`${API_URL}/register`, userData);
            
             return response.data;
         }
-
-
+        
         export const loginUser = async (userData) => {
+
             // const API_URL = process.env.API_URL;
 
             //new way to access env variables in Vite
@@ -22,7 +25,8 @@
             return response.data;
         }
 
-       export const fetchUserProfile = async () => {
+        export const fetchUserProfile = async () => {
+
         const token = localStorage.getItem('token');
         const API_URL = import.meta.env.VITE_API_URL;
         const response = await axios.get(`${API_URL}/profile`, {
@@ -30,6 +34,6 @@
                 Authorization: `Bearer ${token}`,       
 
        }  
-        });
+        }); 
         return response.data;
        }
